21d60f62f4141e2b3c02e69547579480b70fa843
Marco Lau <lau.career@gmail.com>
2013-09-22 15:19:46 +0800

    Setup GUI layout

--- gen/com/example/stockquote/R.java
+++ gen/com/example/stockquote/R.java
@@ -10,6 +10,11 @@ package com.example.stockquote;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aero_blue=0x7f040000;
+        public static final int black=0x7f040002;
+        public static final int egg_shell=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,47 +22,62 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int stock_list_text_size=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TableLayout1=0x7f080000;
-        public static final int action_settings=0x7f080001;
+        public static final int TableLayout1=0x7f090000;
+        public static final int action_settings=0x7f09000e;
+        public static final int deleteStocksButton=0x7f09000a;
+        public static final int enterStockSymbolButton=0x7f090004;
+        public static final int enterSymbolTextView=0x7f090002;
+        public static final int quoteFromWebButton=0x7f09000d;
+        public static final int stockQuoteButton=0x7f09000c;
+        public static final int stockScrollView=0x7f090008;
+        public static final int stockSymbolEditText=0x7f090003;
+        public static final int stockSymbolTextView=0x7f090006;
+        public static final int tableRow1=0x7f090001;
+        public static final int tableRow2=0x7f090005;
+        public static final int tableRow3=0x7f090007;
+        public static final int tableRow4=0x7f090009;
+        public static final int textView1=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int stock_quote_row=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int cancel=0x7f050007;
-        public static final int change=0x7f050016;
-        public static final int change_stock_symbol=0x7f050005;
-        public static final int confirm_action=0x7f05000b;
-        public static final int days_range=0x7f050017;
-        public static final int delete_all_symbols=0x7f05000a;
-        public static final int enter_stock_symbol=0x7f050003;
-        public static final int get_stock_quote=0x7f05000c;
-        public static final int go_to_website=0x7f05000d;
-        public static final int invalid_stock_symbol=0x7f050009;
-        public static final int last_trade_price_only=0x7f050015;
-        public static final int missing_stock_symbol=0x7f050008;
-        public static final int ok=0x7f050006;
-        public static final int stock_company_name=0x7f05000f;
-        public static final int stock_days_high=0x7f050013;
-        public static final int stock_days_low=0x7f050012;
-        public static final int stock_symbol=0x7f050002;
-        public static final int stock_symbol_list=0x7f050004;
-        public static final int stock_year_high=0x7f050011;
-        public static final int stock_year_low=0x7f050010;
-        public static final int title_activity_stock_info=0x7f050014;
-        public static final int yahoo_stock_url=0x7f05000e;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int cancel=0x7f060007;
+        public static final int change=0x7f060016;
+        public static final int change_stock_symbol=0x7f060005;
+        public static final int confirm_action=0x7f06000b;
+        public static final int days_range=0x7f060017;
+        public static final int delete_all_symbols=0x7f06000a;
+        public static final int enter_stock_symbol=0x7f060003;
+        public static final int get_stock_quote=0x7f06000c;
+        public static final int go_to_website=0x7f06000d;
+        public static final int invalid_stock_symbol=0x7f060009;
+        public static final int last_trade_price_only=0x7f060015;
+        public static final int missing_stock_symbol=0x7f060008;
+        public static final int ok=0x7f060006;
+        public static final int stock_company_name=0x7f06000f;
+        public static final int stock_days_high=0x7f060013;
+        public static final int stock_days_low=0x7f060012;
+        public static final int stock_symbol=0x7f060002;
+        public static final int stock_symbol_list=0x7f060004;
+        public static final int stock_year_high=0x7f060011;
+        public static final int stock_year_low=0x7f060010;
+        public static final int title_activity_stock_info=0x7f060014;
+        public static final int yahoo_stock_url=0x7f06000e;
     }
     public static final class style {
         /** 
@@ -81,10 +101,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
