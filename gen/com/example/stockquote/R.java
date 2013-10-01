4f0ec672dd00fe99cd079d51428b2b5d46cffc2a
Marco Lau <lau.career@gmail.com>
2013-10-01 16:01:04 +0800

    Build all UI

--- gen/com/example/stockquote/R.java
+++ gen/com/example/stockquote/R.java
@@ -31,12 +31,12 @@ public final class R {
     }
     public static final class id {
         public static final int TableLayout1=0x7f090000;
-        public static final int action_settings=0x7f09000e;
+        public static final int action_settings=0x7f09000d;
         public static final int deleteStocksButton=0x7f09000a;
         public static final int enterStockSymbolButton=0x7f090004;
         public static final int enterSymbolTextView=0x7f090002;
-        public static final int quoteFromWebButton=0x7f09000d;
-        public static final int stockQuoteButton=0x7f09000c;
+        public static final int quoteFromWebButton=0x7f09000c;
+        public static final int stockQuoteButton=0x7f09000b;
         public static final int stockScrollView=0x7f090008;
         public static final int stockSymbolEditText=0x7f090003;
         public static final int stockSymbolTextView=0x7f090006;
@@ -44,7 +44,6 @@ public final class R {
         public static final int tableRow2=0x7f090005;
         public static final int tableRow3=0x7f090007;
         public static final int tableRow4=0x7f090009;
-        public static final int textView1=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
