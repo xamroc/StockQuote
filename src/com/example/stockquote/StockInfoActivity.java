311b58347ba21fdc44388f726bcb3387066bf22d
Marco Lau <lau.career@gmail.com>
2013-10-02 17:25:50 +0800

    Fix bug by adding internet permissions on manifest file

--- src/com/example/stockquote/StockInfoActivity.java
+++ src/com/example/stockquote/StockInfoActivity.java
@@ -56,7 +56,7 @@ public class StockInfoActivity extends Activity{
 	String daysRange = "";
 
 	String yahooURLFirst = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22";
-	String yahooURLSecond = "%22)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	String yahooURLSecond = "%22)&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 	
 	protected void onCreate(Bundle savedInstanceState){
 		
@@ -79,7 +79,7 @@ public class StockInfoActivity extends Activity{
 		
 		final String yqlURL = yahooURLFirst + stockSymbol + yahooURLSecond;
 		
-		new MyAsyncTask().execute();
+		new MyAsyncTask().execute(yqlURL);
 		
 	}
 	
