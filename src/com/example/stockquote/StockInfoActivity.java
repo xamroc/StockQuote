4c23d567146a4a487d20b1507f639edc8d6c903d
Marco Lau <lau.career@gmail.com>
2013-10-07 23:37:44 +0800

    Initialize String[][] xmlPullParserArray and parserArrayIncrement

--- src/com/example/stockquote/StockInfoActivity.java
+++ src/com/example/stockquote/StockInfoActivity.java
@@ -58,6 +58,14 @@ public class StockInfoActivity extends Activity{
 	String yahooURLFirst = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22";
 	String yahooURLSecond = "%22)&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 	
+	String[][] xmlPullParserArray = {{"AverageDailyVolume", "0"},{"Change", "0"},
+			{"DaysLow", "0"},{"DaysHigh", "0"},{"YearLow", "0"},
+			{"YearHigh", "0"},{"MarketCapitalization", "0"},{"LastTradePriceOnly", "0"},
+			{"DaysRange", "0"},{"Name", "0"},{"Symbol", "0"},
+			{"Volume", "0"},{"StockExchange", "0"}};
+	
+	int parserArrayIncrement = 0;
+	
 	protected void onCreate(Bundle savedInstanceState){
 		
 		super.onCreate(savedInstanceState);
@@ -88,65 +96,7 @@ public class StockInfoActivity extends Activity{
 		@Override
 		protected String doInBackground(String... args) {
 			
-			try {
-				
-				URL url = new URL(args[0]);
-				
-				URLConnection connection;
-				connection = url.openConnection();
-				
-				HttpURLConnection httpConnection = (HttpURLConnection) connection;
-				
-				int responseCode = httpConnection.getResponseCode();
-				
-				if(responseCode == HttpURLConnection.HTTP_OK) {
-					
-					InputStream in = httpConnection.getInputStream();
-					
-					DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-					
-					DocumentBuilder db = dbf.newDocumentBuilder();
-					
-					Document dom = db.parse(in);
-					
-					Element docEle = dom.getDocumentElement();
-					
-					NodeList nl = docEle.getElementsByTagName("quote");
-					
-					if(nl != null &&  nl.getLength() > 0) {
-						
-						for(int i=0; i < nl.getLength(); i++){
-							
-							StockInfo theStock = getStockInformation(docEle);
-							
-							name = theStock.getName();
-							yearLow = theStock.getYearLow();
-							yearHigh = theStock.getYearHigh();
-							daysLow = theStock.getDaysLow();
-							daysHigh = theStock.getDaysHigh();
-							lastTradePriceOnly = theStock.getLastTradePriceonly();
-							change = theStock.getChange();
-							daysRange = theStock.getDaysRange();
-							
-						}
-						
-					}
-					
-				}
-				
-			}
 			
-			catch (MalformedURLException e) {
-					Log.d(TAG, "MalformedURLException", e);
-				} catch (IOException e) {
-					Log.d(TAG, "IOException", e);
-				} catch (ParserConfigurationException e) {
-					Log.d(TAG, "Parser Configuration Exception", e);
-				} catch (SAXException e) {
-					Log.d(TAG, "SAX Exception", e);
-				}
-			
-			finally { }
 			
 			return null;
 		}
@@ -164,41 +114,6 @@ public class StockInfoActivity extends Activity{
 			
 		}
 		
-		private StockInfo getStockInformation(Element entry) {
-			
-			String stockName = getTextValue(entry, "Name");
-			String stockYearLow = getTextValue(entry, "YearLow");
-			String stockYearHigh = getTextValue(entry, "YearHigh");
-			String stockDaysLow = getTextValue(entry, "DaysLow");
-			String stockDaysHigh = getTextValue(entry, "DaysHigh");
-			String stockLastTradePriceOnly = getTextValue(entry, "LastTradePriceOnly");
-			String stockChange = getTextValue(entry, "Change");
-			String stockDaysRange = getTextValue(entry, "DaysRange");
-			
-			StockInfo theStock = new StockInfo(stockDaysLow, stockDaysHigh, stockYearLow,
-					stockYearHigh, stockName, stockLastTradePriceOnly,
-					stockChange, stockDaysRange);
-			
-			return theStock;
-		}
-		
-		private String getTextValue(Element entry, String tagName) {
-			
-			String tagValueToReturn = null;
-			
-			NodeList nl = entry.getElementsByTagName(tagName);
-			
-			if(nl != null && nl.getLength() > 0) {
-				
-				Element element = (Element) nl.item(0);
-				
-				tagValueToReturn = element.getFirstChild().getNodeValue();
-				
-			}
-			
-			return tagValueToReturn;
-			
-		}
 		
 	}
 	
